1fbd6a426d28c03252f45dc69deed3ad794051b4
Cauã Pereira <caua.souza@ufv.br>
2026-06-20 22:01:30 -0300

    feature/programacao-dias feature/filtro-programacao fix/date-adapter

--- app/src/main/java/com/example/gesuas360/views/ProgramacaoFragment.java
+++ app/src/main/java/com/example/gesuas360/views/ProgramacaoFragment.java
@@ -34,18 +34,29 @@ public class ProgramacaoFragment extends BaseFragment {
 
         RecyclerView rvProgramacao = view.findViewById(R.id.rv_programacao);
         rvProgramacao.setLayoutManager(new LinearLayoutManager(getContext()));
-        
-        List<Palestra> lista = PalestraRepository.getInstance().getPalestras();
-        
+
+        List<Palestra> lista = PalestraRepository.getInstance().getPalestrasByDay(11);
+
         ProgramacaoAdapter adapter = new ProgramacaoAdapter(lista, palestra -> {
             Navigation.findNavController(view).navigate(R.id.action_programacao_to_detalhes);
         });
-        
+
         rvProgramacao.setAdapter(adapter);
 
         RecyclerView rvDates = view.findViewById(R.id.rv_dates);
         rvDates.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        rvDates.setAdapter(new DateAdapter(Arrays.asList("11", "12", "13", "14")));
+        rvDates.setAdapter(
+                new DateAdapter(
+                        Arrays.asList("11", "12", "13", "14"),
+                        dia -> {
+                            adapter.atualizarPalestras(
+                                    PalestraRepository
+                                            .getInstance()
+                                            .getPalestrasByDay(dia)
+                            );
+                        }
+                )
+        );
     }
 
     @Override
